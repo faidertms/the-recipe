7e31013ebc15d71fe4e72ba1edbed3dca6e23513
Thiago Sales <thiagomsales@hotmail.com>
2021-02-27 00:37:59 -0300

    feat: add route to home page in app.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,7 +1,12 @@
+import HomePage from "./pages/Home";
+
+/**
+ *  due to having only one page will not be used React Router
+ */
 function App(): JSX.Element {
   return (
     <div className="bg-white">
-      init
+      <HomePage />
     </div>
   );
 }
